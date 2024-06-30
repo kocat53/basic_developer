0feb6f24bcdbeefea2c6dc903e3c13f72b14899a
JS.Park <kocat53@naver.com>
2024-07-01 00:12:15 +0900

    [ fe / update ] : 공통 컴포넌트 보완작업

--- fe/src/components/common/Button.vue
+++ fe/src/components/common/Button.vue
@@ -5,26 +5,24 @@
 </template>
 
 <script setup lang="ts">
-import { computed, defineProps, withDefaults } from 'vue'
-
 const props = withDefaults(
   defineProps<{
-    theme?: 'primary' | 'secondary' | 'success' | 'danger' | 'warning'
-    size?: 'lg' | 'sm' | ''
+    theme?: "primary" | "secondary" | "success" | "danger" | "warning";
+    size?: "lg" | "sm" | "";
   }>(),
   {
-    theme: 'secondary',
-    size: ''
+    theme: "secondary",
+    size: "",
   }
-)
+);
 
 const classes = computed(() => {
   return {
     btn: true,
     [`btn-${props.theme}`]: true,
-    [`btn-${props.size}`]: props.size
-  }
-})
+    [`btn-${props.size}`]: props.size,
+  };
+});
 </script>
 
 <style scoped></style>
--- fe/src/components/common/Checkbox.vue
+++ fe/src/components/common/Checkbox.vue
@@ -16,67 +16,68 @@
 </template>
 
 <script setup lang="ts">
-import { useSlots, useAttrs, computed, withDefaults, defineProps, defineOptions } from 'vue'
-const slots = useSlots()
-const $attrs = useAttrs()
-
-const hasContent = !!slots.default
-const emits = defineEmits(['update:modelValue'])
+const slots = useSlots();
+const $attrs = useAttrs();
+const hasContent = !!slots.default;
+const emits = defineEmits(["update:modelValue"]);
 
 defineOptions({
-  inheritAttrs: false
-})
+  inheritAttrs: false,
+});
 
 const props = withDefaults(
   defineProps<{
-    type?: 'checkbox' | 'radio'
-    inline?: boolean
-    value?: string | number | boolean
-    modelValue?: boolean | any[] | string | number
-    id?: string
+    type?: "checkbox" | "radio";
+    inline?: boolean;
+    value?: string | number | boolean;
+    modelValue?: boolean | any[] | string | number;
+    id?: string;
   }>(),
   {
-    type: 'checkbox',
+    type: "checkbox",
     inline: false,
-    value: '',
+    value: "",
     id: `check-${Math.random().toString(36).substr(2, 9)}`,
-    modelValue: false
+    modelValue: false,
   }
-)
+);
 
 // 체크 설정
 const isChecked = computed(() => {
   // 타입이 체크박스이고 배열 value가 넘어올때
-  if (props.type === 'checkbox' && Array.isArray(props.modelValue)) {
-    return props.modelValue.includes(props.value)
+  if (props.type === "checkbox" && Array.isArray(props.modelValue)) {
+    return props.modelValue.includes(props.value);
   }
   // 배열이 아닌 일반 값 일때
-  return props.modelValue === props.value
-})
+  return props.modelValue === props.value;
+});
 
 const updateValue = (event: Event) => {
-  const target = event.target as HTMLInputElement
+  const target = event.target as HTMLInputElement;
 
-  if (props.type === 'checkbox' && Array.isArray(props.modelValue)) {
-    handleCheckboxArray(target.checked)
+  if (props.type === "checkbox" && Array.isArray(props.modelValue)) {
+    handleCheckboxArray(target.checked);
   } else {
-    emits('update:modelValue', target.type === 'checkbox' ? target.checked : target.value)
+    emits(
+      "update:modelValue",
+      target.type === "checkbox" ? target.checked : target.value
+    );
   }
-}
+};
 
 const handleCheckboxArray = (checked: boolean) => {
-  if (!Array.isArray(props.modelValue)) return
-  const newValue = [...props.modelValue]
+  if (!Array.isArray(props.modelValue)) return;
+  const newValue = [...props.modelValue];
   if (checked) {
-    newValue.push(props.value)
+    newValue.push(props.value);
   } else {
-    const index = newValue.indexOf(props.value)
+    const index = newValue.indexOf(props.value);
     if (index > -1) {
-      newValue.splice(index, 1)
+      newValue.splice(index, 1);
     }
   }
-  emits('update:modelValue', newValue)
-}
+  emits("update:modelValue", newValue);
+};
 </script>
 
 <style scoped></style>
