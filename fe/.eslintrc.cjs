4dbec93e32c48d526f0966d61dbe7c923898c329
JS.Park <kocat53@naver.com>
2024-06-30 23:09:10 +0900

    [ fe / add ]  : auto import 라이브러리 설치 및 설정

--- fe/.eslintrc.cjs
+++ fe/.eslintrc.cjs
@@ -6,6 +6,7 @@ module.exports = {
   extends: [
     "plugin:vue/vue3-essential",
     "eslint:recommended",
+    "./.eslintrc-auto-import.json",
     "@vue/eslint-config-typescript",
     "@vue/eslint-config-prettier/skip-formatting",
     "plugin:prettier/recommended",
--- fe/vite.config.ts
+++ fe/vite.config.ts
@@ -1,18 +1,27 @@
-import { fileURLToPath, URL } from 'node:url'
-
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import VueDevTools from 'vite-plugin-vue-devtools'
+import { fileURLToPath, URL } from "node:url";
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import VueDevTools from "vite-plugin-vue-devtools";
+import AutoImport from "unplugin-auto-import/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), VueDevTools()],
+  plugins: [
+    vue(),
+    VueDevTools(),
+    AutoImport({
+      eslintrc: {
+        enabled: true, // <-- this
+      },
+      imports: ["vue", "vue-router"],
+    }),
+  ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
   },
   server: {
-    port: 3000
-  }
-})
+    port: 3000,
+  },
+});
